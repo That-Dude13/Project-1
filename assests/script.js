acbdabeb9a99c73894d4ee1ee8d1973de649ec92
Change Agent <keasiley@icloud.com>
2023-03-30 12:26:38 -0500

    Script file modified to use new API

--- assests/script.js
+++ assests/script.js
@@ -3,35 +3,28 @@
     let searchBtn = document.getElementById('search-btn')
 
 	const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'bebcc43ab2msh18ba74d20be3f39p1a5d43jsn8040bebec351',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
-};
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'bebcc43ab2msh18ba74d20be3f39p1a5d43jsn8040bebec351',
+			'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+		}
+	};
+	function search(){
+ 	var searchTerm = result.value
 
-function search(){
-var searchTerm = result.value
+	fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q='+searchTerm,options)
+		.then(response => response.json())
+		.then(response =>{
+			console.log(response)
+		 console.log(response.results[0])
+		})
+	
+		.catch(err => console.error(err))
 
-fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix='+searchTerm, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error('err'));
+		
+
+	
 }
+
 
 searchBtn.addEventListener('click',search);
-
-const generateRecipes = (data) => {
-	const result = data.results [0]
-
-	const recipeResult = `
-		<div class="card">
-			<div>
-			</div>
-			</div>
-
-		</div>
-	`
-
-
-}
